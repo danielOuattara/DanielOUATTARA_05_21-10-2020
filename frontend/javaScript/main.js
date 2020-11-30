e542f1a0f7477239ac1c9cb9685c5703f7020d19
daniel ouattara <daniel.ouattara@gmx.com>
2020-11-30 17:34:18 +0100

    accueil avec modal ( à retirer)

--- frontend/javaScript/main.js
+++ frontend/javaScript/main.js
@@ -11,12 +11,15 @@ class Produits {
             // ordonner les objets produits par prix croissant
             let infosProduits = data.sort(function(a, b) {
                 return a.price - b.price
-               });
-            // extraire les valeurs individuelle à renseigner
-            infosProduits = infosProduits.map (index => {
-                const { lense, _id, name, description, price, imageUrl} = index;
-                return { lense, _id, name, description, price, imageUrl}
             });
+
+            // extraire les valeurs individuelle à renseigner
+            infosProduits = infosProduits.map (value => {
+                const { lenses, _id, name, description, price, imageUrl} = value;
+                return { lenses, _id, name, description, price, imageUrl}
+            });
+
+            // console.log("lense[0] = ", lenses);
             console.log(" infosProduits = ", infosProduits);
             return infosProduits;
 
@@ -38,6 +41,7 @@ class UI {
         infosProduits.forEach(article => {
             resultat +=  
                         `
+                        <!-- Afficher articles en vente-->
                         <div class="jumbotron d-flex flex-column flex-md-row articles">
                            <div class="flex-fill">
                               <a href="#">
@@ -49,8 +53,92 @@ class UI {
                                 <h2 class="mb-4 mt-4"> ${article.name} </h2>
                                 <p>${article.description}</p> 
                                 <h4>A partir de ${article.price/100}€</h4>
-                                <button class="btn btn-info mt-4" href="#">Découvrir nos modèles</button>
+                                <button class="btn btn-info mt-4" data-toggle="modal" data-target="#myModal" >Découvrir nos modèles (modal)</button>
                             </div>
+                        </div>
+
+
+                        <!-- The Modal -->
+                        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="modal for ${article.name} item" aria-hidden="true">
+                        <div class="modal-dialog modal-lg modal-dialog-centered">
+                            <div class="modal-content">
+
+                            <!-- Modal Header -->
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="modal-titre> Oricamera</h4>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+
+                            <!-- Modal body -->
+                            <div class="modal-body d-flex flex-column flex-md-row">
+
+                                <div class="flex-fill">
+                                <a href="#">
+                                    <img src=${article.imageUrl} width=600 alt="oricono oricamera image ${article.name}" 
+                                            class="img-thumbnail mx-auto d-block">
+                                </a>
+                                </div>
+                                <div class="flex-fill mx-auto text-center">
+                                    <h2 class="mb-4 mt-4"> ${article.name} </h2>
+                                    <p>${article.description}</p> 
+                                    <h4>${article.price/100}€</h4>
+                                </div>
+                            </div>
+
+
+                            <form>
+    
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Modèle</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                     <option>${article.lenses[0]}</option>
+                                     <option>${article.lenses[1]}</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="quantity">Quantity (between 1 and 5):</label>
+                                    <input type="number" id="quantity" name="quantity" min="1" max="99">
+                                </div>
+                            </form>
+
+
+                            <div class="form-group">
+                                <label for="exampleFormControlSelect1">Quantité</label>
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                </select>
+                            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+
+                                    <!-- Modal footer -->
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger" data-dismiss="modal">Fermez</button>
+                                    </div>
+
+                                    </div>
+                                </div>
+                                
+
+
+
+                                
+                            
                         </div> 
                         `;
         });
