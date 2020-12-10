19632b142f71c6c7daa8ab5df5d969dab972bd12
daniel ouattara <daniel.ouattara@gmx.com>
2020-12-10 01:09:31 +0100

    en route regulation option non définie

--- frontend/javaScript/produit.js
+++ frontend/javaScript/produit.js
@@ -24,6 +24,24 @@ async function dataProduit() {
 
  async function afficherData(data) {
         const {lenses, _id, name, price, description, imageUrl } = data;
+
+
+        //MAYDAY MAYDAY MAYDAY-------------------------------------------------
+
+            // Voir camera :  Franck JS 105, "la seconde option" n'est pas prédéfinie
+
+            // if (lenses[1] == null) lenses[1] = "" // --> OK , mais pas élégant: affiche vide           
+
+      
+
+            //regulationOption   
+            // let optionArticle = document.querySelector('.option-article');
+            // if (lenses[1] == null ) optionArticle[1].disabled = "true"  
+        
+        //----------------------------------------------------------------------
+
+    
+        console.log(lenses[0]);
         
 
         produit.innerHTML =  
@@ -58,40 +76,36 @@ async function dataProduit() {
 
                            <div class="form-group form-inline my-5 pl-4">
                                <label for="option"><h4>Lentilles: </h4></label>
-                               <select class="form-control ml-3 col-sm-2" id="option">
-                                 <option>${lenses[0]}</option>
-                                 <option>${lenses[1]}</option>
+                               <select class="form-control ml-3 col-sm-2" class="option-article">
+                                 <option value=${lenses[0]} > ${lenses[0]} </option>
+                                 <option value=${lenses[1]} class="option-article"> ${lenses[1]} </option>
                                </select>
                            </div> 
 
                            <div class="form-group form-inline  pl-4">
                                <label for="quantite"><h4>Quantité : </h4></label>
-                               <select class="form-control ml-3 col-sm-2" id="quantite">
-                                 <option>1</option>
-                                 <option>2</option>
-                                 <option>3</option>
-                                 <option>4</option>
-                                 <option>5</option>
-                                 <option>6</option>
-                                 <option>7</option>
-                                 <option>8</option>
-                                 <option>9</option>
-                                 <option>10</option>
+                               <select class="form-control ml-3 col-sm-2 quantite-article">
+                                 <option value=1> 1 </option>
+                                 <option value=2> 2 </option>
+                                 <option value=3> 3 </option>
+                                 <option value=4> 4 </option>
+                                 <option value=5> 5 </option>
+                                 <option value=6> 6 </option>
+                                 <option value=7> 7 </option>
+                                 <option value=8> 8 </option>
+                                 <option value=9> 9</option>
+                                 <option value=10> 10</option>
                                </select>
                            </div> 
 
-                           <h3>total : <span id= prix-total>${price/100}</span>€</h3>
+                           <h3>total : <span id="prix-total">${price/100}</span>€</h3>
                            <button  class="btn btn-info mt-4" href="#">Ajoutez au panier</button>
                        </div>
                    </div>
                </div>
                     `;
 
-
-    let prixTotal = document.querySelector('#prix-total')
-    let quantiteChoisie = document.querySelector('#quantite');
-
-
+    // Opacity 50% des images 
     let image_2 = document.getElementById('hover-image')
     image_2.addEventListener('mouseover', function() {
         this.style.opacity ='0.7'
@@ -99,7 +113,16 @@ async function dataProduit() {
     image_2.addEventListener('mouseout', function() {
         this.style.opacity ='1'
     });
-            
+
+    //  Calcul du prix total selon quantité articles choisies
+    let prixTotal = document.querySelector('#prix-total')
+    let quantiteArticle = document.querySelector('.quantite-article');
+    quantiteArticle.addEventListener('change', (event)=> {
+        prixTotal.textContent  = event.target.value * price/100;
+    });
+
+
+                
 };
 
 
